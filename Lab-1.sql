9751fbf625c82a25d3dbd532c9cb9df497239002
AmrSalem101 <amr.salem@tu-dortmund.de>
2024-03-02 11:18:45 +0100

    Create Lab-1.sql

---
+++ Lab-1.sql
@@ -0,0 +1,46 @@
+#1
+SELECT 
+	*
+from 
+	actor;
+----------
+SELECT 
+	*
+from 
+	 film ;
+	
+----------
+SELECT 
+	*
+from 
+	customer;
+----------
+#2
+select 
+	title
+from 
+	film_actor;
+----------
+#3
+select distinct
+	language_id
+from
+	language;
+----------
+#5.1 
+SELECT distinct
+	store_id
+FROM 
+	sakila.store;
+----------
+#5.2
+SELECT distinct	
+	staff_id
+FROM 
+	sakila.staff;
+----------
+#5.3
+SELECT distinct	
+	first_name
+FROM 
+	sakila.staff;
